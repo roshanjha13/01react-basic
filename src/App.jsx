adccd69f12ac37b30954cdd5fdbb761014ffd56f
roshanjha31 <jharoshan618@gmail.com>
2023-09-17 12:09:39 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import Chai from "./chai"
+function App() {
+  return (
+    <Chai />
+  )
+}
+
+export default App
+
+// in react we return only one element 
+// so we wrap in single div or react fragment tag
+//in two or more element
+//component name should be capital
